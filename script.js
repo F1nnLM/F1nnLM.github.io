b04111f5067c7bf3c50f2c9f1f7181d7adf4cece
Ludovico Marino <113300950+F1nnLM@users.noreply.github.com>
2023-01-03 22:06:25 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("mySidepanel").style.width = "350px";
-}
-
-
-function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-}
